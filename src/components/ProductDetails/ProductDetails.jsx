5832c537ade3362fd4e819f0cba220e9ae128011
lijanahmmed <lijanahmmed@gmail.com>
2025-11-05 00:04:57 +0600

    show product bid details in table

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,11 +1,22 @@
-import React, { use, useRef } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import { useLoaderData } from "react-router";
 import { AuthContext } from "../../contexts/AuthContext";
+import Swal from "sweetalert2";
 
 const ProductDetails = () => {
   const { _id: productId } = useLoaderData();
+  const [bids, setBids] = useState([]);
   const bidModalRef = useRef(null);
   const { user } = use(AuthContext);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/products/bids/${productId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("bids for this product", data);
+        setBids(data);
+      });
+  }, [productId]);
 
   const handleBidModalOpen = () => {
     bidModalRef.current.showModal();
@@ -36,21 +47,21 @@ const ProductDetails = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        // if (data.insertedId) {
-        //   bidModalRef.current.close();
-        //   Swal.fire({
-        //     position: "top-end",
-        //     icon: "success",
-        //     title: "Your bid has been placed.",
-        //     showConfirmButton: false,
-        //     timer: 1500,
-        //   });
-        //   // add the new bid to the state
-        //   newBid._id = data.insertedId;
-        //   const newBids = [...bids, newBid];
-        //   newBids.sort((a, b) => b.bid_price - a.bid_price);
-        //   setBids(newBids);
-        // }
+        if (data.insertedId) {
+          bidModalRef.current.close();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your bid has been placed.",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          //   // add the new bid to the state
+            newBid._id = data.insertedId;
+            const newBids = [...bids, newBid];
+            newBids.sort((a, b) => b.bid_price - a.bid_price);
+            setBids(newBids);
+        }
       });
   };
 
@@ -104,17 +115,66 @@ const ProductDetails = () => {
               </form>
 
               <div className="modal-action">
-                <form method="dialog">
-                  {/* if there is a button in form, it will close the modal */}
+                {/* <form method="dialog">
+                  if there is a button in form, it will close the modal
                   <button className="btn">Cancel</button>
-                </form>
+                </form> */}
               </div>
             </div>
           </dialog>
         </div>
       </div>
       {/* bids for this product */}
-      <div></div>
+      <div>
+        <h3 className="text-3xl">
+          Bids for this Product:{" "}
+          <span className="text-primary">{bids.length}</span>
+        </h3>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>SL No.</th>
+                <th>Buyer Name</th>
+                <th>Buyer Email</th>
+                <th>Bid Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {bids.map((bid, index) => (
+                <tr key={index}>
+                  <th>{index + 1} </th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{bid.buyer_name}</div>
+                        <div className="text-sm opacity-50">United States</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{bid.buyer_email}</td>
+                  <td>${bid.bid_price}</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                  </th>
+                </tr>
+              ))}
+              {/* row 2 */}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
